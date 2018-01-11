4e0a074bf38054a076f7af625268947acae67a9b
peoray <peoray@yahoo.com>
2018-01-11 02:09:32 +0100

    Add checkbox usage

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('connected');
+var fact = document.getElementById('fact');
+
+var factText = document.getElementById('factText');
+
+var numberInput = document.getElementById('numberInput');
+
+numberInput.addEventListener('input', getFact);
+
+function getFact() {
+	var number = numberInput.value;
+
+	var xhr = new XMLHttpRequest();
+
+	if (document.getElementById('year').checked) {
+		xhr.open('GET', 'http://numbersapi.com/'+number+'/year', true);
+	}
+
+	else if (document.getElementById('date').checked) {
+		xhr.open('GET', 'http://numbersapi.com/'+number+'/date', true);
+	}
+
+	else {
+		xhr.open('GET', 'http://numbersapi.com/'+number, true);
+	}
+
+	xhr.onload = function() {
+		if(this.status === 200 && number !== '') {
+			console.log(this.responseText);
+			factText.textContent = this.responseText;
+		}
+	}
+
+	xhr.send();
+}
+
