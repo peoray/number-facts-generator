1a1552017845aa0582d3c7b5ccce0a7b5480def7
peoray <peoray@yahoo.com>
2018-01-26 22:23:06 +0100

    Add CDNs for Bootstrap and Jquery

--- script.js
+++ script.js
@@ -10,77 +10,77 @@ tweetButton.addEventListener('click', tweetFact);
 
 function getFact() {
 
-	// GET FACT CONTAINER
-	var fact = document.getElementById('fact');
+    // GET FACT CONTAINER
+    var fact = document.getElementById('fact');
 
-	// GET VALUE OF FORM INPUT
-	var number = numberInput.value;
+    // GET VALUE OF FORM INPUT
+    var number = numberInput.value;
 
-	var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 
-	if (document.getElementById('year').checked) {
-		xhr.open('GET', 'http://numbersapi.com/'+number+'/year', true);
-	}
+    if (document.getElementById('year').checked) {
+        xhr.open('GET', 'http://numbersapi.com/' + number + '/year', true);
+    } else if (document.getElementById('date').checked) {
+        xhr.open('GET', 'http://numbersapi.com/' + number + '/date', true);
+    } else {
+        xhr.open('GET', 'http://numbersapi.com/' + number, true);
 
-	else if (document.getElementById('date').checked) {
-		xhr.open('GET', 'http://numbersapi.com/'+number+'/date', true);
-	}
+    }
 
-	else {
-		xhr.open('GET', 'http://numbersapi.com/'+number, true);
-	}
+    xhr.onload = function() {
+        if (this.status === 200 && number !== '') {
+            fact.style.display = 'block';
+            factText.textContent = this.responseText;
+        }
+    }
 
-	xhr.onload = function() {
-		if(this.status === 200 && number !== '') {
-			fact.style.display = 'block';
-			factText.textContent = this.responseText;
-		}
-	}
-
-	xhr.send();
+    xhr.send();
 }
 
 function getFocused() {
 
-	var radios = document.querySelectorAll('input[type="radio"]');
+    var radios = document.querySelectorAll('input[type="radio"]');
 
-	radios.forEach(function() {
-		this.addEventListener('click', function() {
-			document.querySelector('input[type="text"]').focus();
-			document.querySelector('input[type="text"]').value = '';
-			// factText.style.display = 'none';
-		});
+    radios.forEach(function() {
+        this.addEventListener('click', function() {
+            document.querySelector('input[type="text"]').focus();
+            document.querySelector('input[type="text"]').value = '';
+            document.getElementById('fact').style.display = 'none';
 
-	})
-
+        })
+    })
 }
 
 getFocused();
 
+// window.addEventListener('click', ()=>{
+// document.querySelector('input[type="text"]').focus();
+//             document.querySelector('input[type="text"]').value = '';
+//                         document.getElementById('fact').style.display = 'none';
+       
+// });
 
 function check() {
-	// GET DATE ALERT
-	var showDate = document.getElementById('showDate');
+    // GET DATE ALERT
+    var showDate = document.getElementById('showDate');
 
-	var radioButton = document.getElementsByClassName("radio");
+    var radioButton = document.getElementsByClassName("radio");
 
-	this.addEventListener('click', function() {
-		if (radioButton[2].checked) {
-			showDate.style.display = 'block';
-		}
-
-		else {
-			showDate.style.display = 'none';
-		}
-	})
+    this.addEventListener('click', function() {
+        if (radioButton[2].checked) {
+            showDate.style.display = 'block';
+        } else {
+            showDate.style.display = 'none';
+        }
+    })
 }
 
 check();
 
 function tweetFact() {
-	var url = "https://twitter.com/intent/tweet";
-	var text = factText.innerHTML;
-	var hashtags = "100DaysOfCode, CodeNewbie";
-	var via = "super_raay";
-	window.open(url+"?text="+text+";hashtags="+hashtags+";via="+via,"","width=500, height=300");
+    var url = "https://twitter.com/intent/tweet";
+    var text = factText.innerHTML;
+    var hashtags = "100DaysOfCode, CodeNewbie";
+    var via = "super_raay";
+    window.open(url + "?text=" + text + ";hashtags=" + hashtags + ";via=" + via, "", "width=500, height=300");
 }
